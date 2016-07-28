cf5a4e064a7d2bad31f3d39ee270f5d2d106bd51
Ryan Grigsby <rgrigsb@gmail.com>
2016-07-28 10:05:46 -0400

    initial commit

---
+++ SubtitleTest/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  SubtitleTest
+//
+//  Created by Ryan Grigsby on 7/27/16.
+//  Copyright © 2016 Grigs-b. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    @IBOutlet var stackView: UIStackView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        guard let playerURL = NSURL(string: "https://github.com/Grigs-b/subtitle-test/subtitle-test.m3u8") else {
+            fatalError("Unable to create URL")
+        }
+        let urlAsset = AVURLAsset(URL: playerURL)
+        urlAsset.mediaSelectionGroupForMediaCharacteristic(AVMediaCharacteristicLegible)?.options.forEach { (option) in
+            let label = UILabel()
+            label.text = option.displayName
+            stackView.addArrangedSubview(label)
+        }
+        
+    }
+
+    
+}
+
